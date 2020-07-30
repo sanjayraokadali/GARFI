ee586164f11495b61bb0974ebe8f596cdeca6b8c
sanjayraokadali <raosanjayraokadali@users.noreply.github.com>
2020-07-30 17:05:35 +0100

    Updated views.py file

--- django CRUD practice/CBV/App/views.py
+++ django CRUD practice/CBV/App/views.py
@@ -5,7 +5,7 @@ from App.models import User
 # Create your views here.
 class BaseView(TemplateView):
 
-    template_name = 'App/base.html'
+template_name = 'App/base.html'
 
 class NextView(CreateView):
 
@@ -15,11 +15,3 @@ class NextView(CreateView):
     context_object_name = 'form'
 
     template_name = 'App/user_form.html'
-
-def DisplayView(request):
-
-    if request.method == 'POST':
-
-        user = User.objects.all()
-        d = {'form':user}
-        return render(request,'App/user_display.html', context = d)
